af59093768708e86ad9deea23c124a0b8cb27da1
lima <lima@mbp>
2023-03-30 16:10:57 -0500

    adding a var

--- main.tf
+++ main.tf
@@ -80,3 +80,8 @@ resource "aws_security_group" "web-sg" {
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
 }
+
+variable "test" {
+  type = string
+  value = "this is a test"
+}
